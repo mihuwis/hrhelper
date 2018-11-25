4653f6dc4d41fc77710f9734cea75eb286be8f4b
Michal Wisniewski <mihuwis@gmail.com>
2018-11-25 14:11:02 +0100

    Initialize class

---
+++ src/main/java/com/mihuwis/hrhelper/View/MassagePrinter.java
@@ -0,0 +1,13 @@
+package com.mihuwis.hrhelper.View;
+
+public class Printer {
+
+    public void printMenuOptions() {
+        System.out.println("Menu: \n1. Print all Users of one type:\n" +
+                "2. Find user by Name or Last Name:\n" +
+                "3. Find user by email alias:\n" +
+                "4. Remove user by Id number");
+    }
+
+    public void
+}
---
+++ src/main/java/com/mihuwis/hrhelper/controlers/MainControler.java
@@ -0,0 +1,4 @@
+package com.mihuwis.hrhelper.controlers;
+
+public class MainControler {
+}
