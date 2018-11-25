3d82ec7b03c0d9d769e43fa5e74cc32df80402cf
Michal Wisniewski <mihuwis@gmail.com>
2018-11-25 14:15:18 +0100

    Initialize class and start RunMenu method

--- src/main/java/com/mihuwis/hrhelper/controlers/MainControler.java
+++ src/main/java/com/mihuwis/hrhelper/controlers/MainControler.java
@@ -1,4 +1,18 @@
 package com.mihuwis.hrhelper.controlers;
 
+import com.mihuwis.hrhelper.View.MassagePrinter;
+
 public class MainControler {
+
+    MassagePrinter massagePrinter;
+
+
+    public MainControler() {
+        this.massagePrinter = new MassagePrinter();
+    }
+
+    public void runMenu(){
+        massagePrinter.printMenuOptions();
+    }
+
 }
